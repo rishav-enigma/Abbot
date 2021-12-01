4d4cff54fea49ac81a06b63651851f4749e4e491
Rishav Kumar <rishav.kumar@anchanto.com>
2021-12-01 16:58:01 +0530

    Removed Gem's dependency

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,5 @@ module Abbot
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.autoload_paths << "#{Rails.root}/app/services"
-    gem_dir = Gem::Specification.find_by_name("protosphere").gem_dir
-    Dir.glob("#{gem_dir}/lib/proto/**/*_services_pb.rb").each { |proto| load proto }
   end
 end
